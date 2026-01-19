6f3cf131c0975507a45fec711351f966b82d3523
adela miclea <adelamiclea709@yahoo.com>
2026-01-19 21:34:48 +0200

    context

---
+++ src/context.js
@@ -0,0 +1,18 @@
+import React, { createContext, useContext, useState } from 'react'
+import cartItems from "./data"
+
+const AppContext=createContext()
+const AppProvider=({children})=>{
+        const [cart,setCart]=useState(cartItems);
+        return (
+            <AppContext.Provider value={{cart}}>
+            {children}
+            </AppContext.Provider>
+        )
+}
+
+export const useGlobalContext=()=>{
+    return useContext(AppContext)
+}
+
+export {AppContext,AppProvider}
