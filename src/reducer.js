1d8c12bbe0189b5e22a5c1b55549e20697c260e4
adela miclea <adelamiclea709@yahoo.com>
2026-01-29 19:08:47 +0200

    reducer

--- src/reducer.js
+++ src/reducer.js
@@ -24,6 +24,43 @@ const reducer=(state,action)=>{
         }).filter((CartItem)=>CartItem!==0);
         return {...state,cart:tempCart}
     }
+    if(action.type==="GetTotals"){
+        let {total,amount}=state.cart.reduce(
+            (CartItem,cartTotal)=>{
+                const {price,amount}=CartItem;
+                const itemTotal=price*amount
+            cartTotal.total+=itemTotal;
+            cartTotal.amount+=amount;
+            },
+            {
+                total:0,
+                amount:0,
+            }
+        );
+        total=parseFloat(total.toFixed(2))
+        return {...state,total,amount}
+    }
+    if(action.type==="Display"){
+        return{...state,cart:action.payload, loading:false}
+    }
+    if(action.type==="Loading"){
+        return{ ...state,loading:true}
+    }
+    if(action.type==="Toggle"){
+        let tempCart=state.cart.map((CartItem)=>{
+            if(CartItem.id===action.payload){
+                if(action.payload.type==="inc"){
+                return {...CartItem,amount:CartItem.amount+1}
+            }
+            if(action.payload.type==="dec"){
+                return {...CartItem,amount:CartItem.amount-1}
+            }
+            }            return CartItem;
+
+        }).filter((CartItem)=>CartItem.amount!==0);
+        return {...state,cart:tempCart}
+    }
+    throw new Error("nu match")
 }
 
 export default reducer
